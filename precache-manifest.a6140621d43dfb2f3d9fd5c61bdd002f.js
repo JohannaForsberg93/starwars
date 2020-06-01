3505bffaba4e1b8bb4bcc570272ff8567f08dd4a
JohannaForsberg93 <johanna_forsberg@msn.com>
2020-06-02 00:16:28 +0200

    Updates

---
+++ precache-manifest.a6140621d43dfb2f3d9fd5c61bdd002f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14b2d3bc304904cc37d419248d36cf24",
+    "url": "/starwars/index.html"
+  },
+  {
+    "revision": "6b645fd066ccdc6ff59a",
+    "url": "/starwars/static/css/main.df70eed6.chunk.css"
+  },
+  {
+    "revision": "a6b2ace118cb1cd9e89d",
+    "url": "/starwars/static/js/2.3e397fba.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/starwars/static/js/2.3e397fba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b645fd066ccdc6ff59a",
+    "url": "/starwars/static/js/main.8fdd44c1.chunk.js"
+  },
+  {
+    "revision": "8836e208f1ad9d492109",
+    "url": "/starwars/static/js/runtime-main.abdb2ecb.js"
+  }
+]);
